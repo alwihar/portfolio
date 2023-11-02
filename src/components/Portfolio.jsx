a15db85b10c0b9ecf5ebd949957e1db1615f2879
Alwina Harutyunyan <alvina.harutyunyan@qiibee.com>
2023-11-02 14:43:59 +0400

    resolved merge conflicts

---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Tech, Projects } from './index'
+
+const Portfolio = () => {
+  return (
+    <>
+      <Projects/>
+      <Tech/>
+    </>
+  );
+};
+
+export default Portfolio;
